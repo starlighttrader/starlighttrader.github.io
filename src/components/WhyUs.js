5b09ef307e2274e3b7991f276ffbd5e2033862c4
Dilip Rajkumar <diliprajkumar@gmail.com>
2025-02-22 21:54:04 +0530

    UpdatedSiteLogo for QuantCopier
    QCLogoChange

--- src/components/WhyUs.js
+++ src/components/WhyUs.js
@@ -16,7 +16,7 @@ const WhyUs = () => {
     },
     { 
       name: "QuantCopier", 
-      url: "#",
+      url: "https://quanttradertools.github.io/quantcopier",
       description: "Our in-house trade copier built for both Discord and Telegram",
       logo: "/QuantCopierLogo.png"
     }
